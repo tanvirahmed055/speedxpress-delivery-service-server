a501463fc10f3dd2e9acfa52c5464909e15d79e7
tanvirahmed055 <ahmedtanvir6168@gmail.com>
2021-10-30 01:17:57 +0600

    made connection with database

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors')
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+
 const app = express()
 const port = 5000
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oa9tu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("yee db connected");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
